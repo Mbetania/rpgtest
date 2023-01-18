8523ea418277c28970a6b21bf6073a31589c68f8
Mbetania <betaniagonzalez31@gmail.com>
2023-01-18 17:29:50 -0300

    si llevamos la config a index funciona correcto

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express")
+import {conexion} from './db.js'
 const app = express()
-const userRouter = require("./app/routes/user")
-const User = require('./app/models/user');
+// const userRouter = require("./app/routes/user")
+// const User = require('./app/models/user');
 require("dotenv").config();
 
-
-
-
-app.use(express.json())
+// app.use(express.json())
 const port = 3001
 
 app.listen(port, () =>{
@@ -17,8 +15,10 @@ app.listen(port, () =>{
 app.get("/", (req, res) =>{
     res.send("hello")
 })
-app.use(userRouter)
+app.get("/ping", async (req,res) => {
+    const result =  await conexion.query('SELECT * FROM userData' )
+    res.json(result)
+})
+// app.use(userRouter)
 
-
-
-User.sync();
+// User.sync();
