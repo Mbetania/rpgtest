1aa2e2377c629ce049ba62ca4ff32ba07290a95f
Mbetania <betaniagonzalez31@gmail.com>
2023-01-25 01:00:05 -0300

    put, delete getUser

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -2,27 +2,34 @@ import { pool } from '../../db.js'
 
 
 
-export const getUsers = async (req, res) => { 
+export const getUsers = async (req, res,next) => { 
     try {
         const [rows] = await pool.query('SELECT * FROM users_data')
-        res.json(rows)
+        return new Promise((resolve, reject) => {
+        resolve(res.json(rows))
+        })
     } catch (error) {
         next(error);
+    }
+}
+
+export const getUser = async (req, res) =>{
+    try{
+        //execute = metodo de mysql2 p/ eject consulta sql en db mysql -> simil query
+        const [user] = await pool.execute('SELECT * FROM users_data WHERE id = ?', [req.params.id])
+        if (!user.length) throw new Error('El usuario no existe')
+        res.json(user)
+    } catch(error) {
+        res.status(404).json({message: error.message})
     }
 }
 
 export const createUser = async (req,res, next) => {
     try{
         let { username, stage, chest, weapon } = req.body;
-        if(stage === undefined || stage === null){
-            stage = 'limbo';
-        }
-        if(chest === undefined || chest === null){
-            chest = 'no items';
-        }
-        if(weapon === undefined || weapon === null){
-            weapon = 'fists';
-        }
+        stage = stage || 'limbo';
+        chest = chest || 'no items';
+        weapon = weapon || 'fists';
         const rows = await pool.query('INSERT INTO users_data (username, stage, chest, weapon) values (?,?,?,?)', [username, stage, chest, weapon])
         res.status(201).json({
             id: rows.insertId,
@@ -36,8 +43,32 @@ export const createUser = async (req,res, next) => {
     }
 }
 
-export const updateUsers = (req,res) => res.send('actualizando users')
-export const deleteUsers = (req,res) => res.send('eliminando users')
+export const deleteUsers = async (req,res) => {
+    const [result] = await pool.query('DELETE FROM users_data WHERE id = ?', [req.params.id])
+    if (result.affectedRows <= 0) return res.status(404).json({
+        message :'No se encuentra el usuario a eliminar'
+    })
+    console.log(result)
+    res.sendStatus(204)
+}
+
+
+
+export const updateUsers = async (req, res, next) => {
+    try{
+        const { id } = req.params
+        const {username, stage, chest, weapon } = req.body
+        const [result] = await pool.query('UPDATE users_data SET username = ?, stage = ?, chest = ?, weapon = ? WHERE id = ?', 
+        [username, stage, chest, weapon, id])
+        if (result.affectedRows <= 0 ) return res.status(404).json({
+            message: "No se encuentra el usuario a actualizar"
+        })
+        res.sendStatus(200)
+        console.log(result)
+    } catch (error){
+        next(error)
+    }    
+}
 
 
 
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {  Router } from "express";
-import { getUsers,createUser, updateUsers, deleteUsers } from '../controllers/user.controllers.js'
+import { getUsers,createUser, updateUsers, deleteUsers, getUser } from '../controllers/user.controllers.js'
 
 const router = Router()
 router.get('/users', getUsers)
+router.get('/user/:id', getUser)
 router.post('/users', createUser)
-router.put('/users', updateUsers)
-router.delete('/users', deleteUsers)
+router.put('/user/:id', updateUsers)
+router.delete('/user/:id', deleteUsers)
 
 export default router
